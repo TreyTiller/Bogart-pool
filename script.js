915893d483eb45b89c6228b094121fa2244bbad1
Trey <treytiller@outlook.com>
2020-06-21 16:45:53 -0400

    Added more page content, Scripts for modals and information. Integrated google forms

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+document.addEventListener("scroll", () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+});
 
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
 
-document.addEventListener('scroll', () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-  });
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+var popup = document.querySelector(".popup");
+var click = document.querySelector(".click");
+var close = document.querySelector(".close");
+
+function toggleModal2() {
+  popup.classList.toggle("reveal-modal");
+}
+
+function windowOnClick2(event) {
+  if (event.target === popup) {
+    toggleModal2();
+  }
+}
+
+click.addEventListener("click", toggleModal2);
+close.addEventListener("click", toggleModal2);
+window.addEventListener("click", windowOnClick2);
